98802f19757e9f07f73d9e7459805d95e958da34
Vladimir Revyagin <skorabeyf@gmail.com>
2024-11-01 11:48:41 +0300

    chore: небольшие правки

--- app/game1.js
+++ app/game1.js
@@ -60,9 +60,6 @@ function resetGame() {
   for (const resetPara of resetParas) {
     resetPara.textContent = ''
   }
-
-  resetButton.parentNode.removeChild(resetButton)
-
   guessField.disabled = false
   guessSubmit.disabled = false
   guessField.value = ''
